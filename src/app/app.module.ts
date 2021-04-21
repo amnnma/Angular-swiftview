a9b89191a409ef481de37851417a6aae13623862
amnnma <umsuperza14@gmail.com>
2021-04-21 13:12:00 +0700

    add file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { LogoComponent } from './logo/logo.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { SearchComponent } from './search/search.component';
+import { TopComponent } from './top/top.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    LogoComponent,
+    FooterComponent,
+    MainComponent,
+    SearchComponent,
+    TopComponent
   ],
   imports: [
     BrowserModule
